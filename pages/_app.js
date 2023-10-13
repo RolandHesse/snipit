0bfeb1e2348dc664157c12b125a4726e51963cf9
Roland Hesse <rolandhesse11783@gmail.com>
2023-10-13 13:05:25 +0200

    create snippet-card component and fetch data from api in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
 export default function App({ Component, pageProps }) {
+  const { data: snippets, error, isLoading } = useSWR("api/snippets", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} data={snippets} />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-export default function HomePage() {
+export default function HomePage({ data }) {
+  console.log("data: ", data);
+
   return (
     <div>
       <h1>Hello from Next.js</h1>
+      {data.map((snippet) => (
+        <p key={snippet._id}>{snippet.name}</p>
+      ))}
     </div>
   );
 }
