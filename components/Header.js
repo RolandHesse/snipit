a3d14903a4209d522c07fe49ce637bb02a59a847
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-10-16 11:43:10 +0200

    add header component and styling

---
+++ components/Header.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+function Header() {
+  return <Logo>/ˈsnɪp.ɪt/</Logo>;
+}
+
+export default Header;
+
+const Logo = styled.h1`
+  display: flex;
+  position: fixed;
+  color: white;
+  background-color: #005f6a;
+  justify-content: center;
+  margin: 0 0 1rem 0;
+  padding: 0.5rem;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 1;
+`;
