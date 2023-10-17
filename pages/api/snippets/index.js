a281600c95cd78b46f0d4e0d9c56d7f4abf16f7e
ThePeet <87306048+PeetThesing@users.noreply.github.com>
2023-10-17 09:13:44 +0200

    Change try and catch statement in pages/api/snippets/index.js
    Co-authored-by: Niklas Röttger <98757391+NikRoe@users.noreply.github.com>

--- pages/api/snippets/index.js
+++ pages/api/snippets/index.js
@@ -4,13 +4,14 @@ import Snippet from "@/db/models/Snippet";
 export default async function handler(request, response) {
   try {
     await dbConnect();
-  } catch (error) {
-    console.error("An error occurred:", error);
-    response.status(500).json({ error: "Internal Server Error" });
-  }
-
+ 
   if (request.method === "GET") {
     const snippets = await Snippet.find();
     return response.status(200).json(snippets);
+  }
+  
+   } catch (error) {
+    console.error("An error occurred:", error);
+    response.status(500).json({ error: "Internal Server Error" });
   }
 }
