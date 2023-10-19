cefa33065b9b333bdb6800fe17b694ad75119a15
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-10-19 13:09:12 +0200

    try to implement message for succsessfully added

--- components/SnippetForm.js
+++ components/SnippetForm.js
@@ -24,58 +24,54 @@ function SnippetForm() {
   }
 
   return (
-    <>
-      <StyledForm onSubmit={handleSubmit}>
-        <h1>Add new Snippet</h1>
-        {/* <fieldset aria-describedby="name">
-          <legend>Enter your Code name and Snippet here</legend> */}
-        <label for="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          placeholder="Code name"
-        ></input>
-        <label for="code">Code:</label>
-        <textarea
-          type="text"
-          id="code"
-          name="code"
-          rows="5"
-          placeholder="your code"
-        ></textarea>
-        {/* </fieldset> */}
-        <label for="description">Description:</label>
-        <textarea
-          type="text"
-          id="description"
-          name="description"
-          rows="5"
-          placeholder="description of the code"
-        ></textarea>
-        <label for="link">Link:</label>
-        <input
-          type="text"
-          id="link"
-          name="link"
-          placeholder="type your link here"
-        ></input>
+    <StyledForm onSubmit={handleSubmit}>
+      <h2>Add new Snippet</h2>
+      <label for="name">Name:</label>
+      <input type="text" id="name" name="name" placeholder="Code name"></input>
+      <label for="code">Code:</label>
+      <textarea
+        type="text"
+        id="code"
+        name="code"
+        rows="5"
+        placeholder="your code"
+      ></textarea>
 
-        <label for="tag">Tag:</label>
-        <select id="tag" name="tag" placeholder="tag">
-          <option value=""></option>
-          <option value="html">html</option>
-          <option value="javaScript">javaScript</option>
-          <option value="next">next</option>
-        </select>
+      <label for="description">Description:</label>
+      <textarea
+        type="text"
+        id="description"
+        name="description"
+        rows="5"
+        placeholder="description of the code"
+      ></textarea>
+      <label for="link">Link:</label>
+      <input
+        type="text"
+        id="link"
+        name="link"
+        placeholder="type your link here"
+      ></input>
 
-        <button type="submit">Submit</button>
-      </StyledForm>
-    </>
+      <label for="tag">Tag:</label>
+      <select id="tag" name="tag" placeholder="tag">
+        <option value=""></option>
+        <option value="html">html</option>
+        <option value="javaScript">javaScript</option>
+        <option value="next">next</option>
+      </select>
+
+      <button type="submit">Submit</button>
+    </StyledForm>
   );
 }
 
 export default SnippetForm;
+
+// StyledForm.addEventListener("submit", (event) => {
+//   event.preventDefault();
+//   return <div>Added Snippet successfuly! </div>;
+// });
 
 const StyledForm = styled.form`
   display: flex;
