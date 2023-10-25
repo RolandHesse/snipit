6fd9a7feacd85a5d6f0f694d1a2736343e80f8a6
Roland Hesse <rolandhesse11783@gmail.com>
2023-10-25 19:16:44 +0200

    refactoring: transferring POST request from SnippetForm component to form.js-page

--- pages/form.js
+++ pages/form.js
@@ -1,11 +1,50 @@
 import BackLink from "@/components/BackLink";
 import SnippetForm from "@/components/SnippetForm";
+import { useState } from "react";
 
 function FormPage() {
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(null);
+
+  async function createSnippet(event, krautsalat) {
+    const response = await fetch("/api/snippets", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(krautsalat),
+    });
+    if (response.ok) {
+      setSubmitted(true);
+      setError(null);
+      event.target.reset();
+      event.target.elements.name.focus();
+    } else {
+      const data = await response.json();
+      setError(data.error);
+    }
+  }
+
   return (
     <>
       <BackLink url={"/"} />
-      <SnippetForm />
+      <SnippetForm onSubmit={createSnippet} />
+      {submitted && (
+        <div>
+          <span role="img" aria-label="check">
+            ✅
+          </span>
+          Added Snippet successfully!
+        </div>
+      )}
+      {error && (
+        <div>
+          <span role="img" aria-label="oh no!">
+            😵‍💫
+          </span>
+          Something went wrong ...
+        </div>
+      )}
     </>
   );
 }
