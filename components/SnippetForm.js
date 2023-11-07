df1bab00b62b0c70964a7e57e50a77a6ea1d98b5
Nadja Kanunnikow <nadja.kanunnikow@gmx.de>
2023-11-07 09:55:33 +0100

    implement warning message with red border fields

--- components/SnippetForm.js
+++ components/SnippetForm.js
@@ -3,23 +3,30 @@ import Button from "./Button";
 import { useState } from "react";
 
 function SnippetForm({ onSubmit, formName, defaultData }) {
-  const [inputValue, setInputValue] = useState("");
-  const [valueLength, setValueLength] = useState(false);
+  const [inputName, setInputName] = useState("");
+  const [inputCode, setInputCode] = useState("");
+  const [warningMessage, setWarningMessage] = useState("");
+  const [formValidated, setFormValidated] = useState(false);
 
-  function checkRequiredFields(event) {
+  function handleInputName(event) {
     const value = event.target.value;
-    setInputValue(value);
+    setInputName(value);
+  }
 
-    if (value.length > 0) {
-      setValueLength(true);
-    } else {
-      setValueLength(false);
-    }
+  function handleInputCode(event) {
+    const value = event.target.value;
+    setInputCode(value);
   }
 
   function handleSubmit(event) {
     event.preventDefault();
 
+    if (inputName === "" || inputCode === "") {
+      setWarningMessage("‼️ Please fill in the required field");
+      setFormValidated(true);
+    } else {
+      setWarningMessage("");
+    }
     const formData = new FormData(event.target);
     const snippetData = Object.fromEntries(formData);
 
@@ -29,30 +36,31 @@ function SnippetForm({ onSubmit, formName, defaultData }) {
   return (
     <StyledForm aria-labelledby={formName} onSubmit={handleSubmit}>
       <h2> {defaultData ? "Update Snippet" : "Add new Snippet"}</h2>
-
-      <Warning>
-        {valueLength ? "‼️ Please fill in the required field" : ""}
-      </Warning>
+      <Warning>{warningMessage}</Warning>
       <p>ℹ️ Fields marked with an * are required</p>
       <label htmlFor="name">Name*</label>
       <StyledInputName
+        value={inputName}
+        onChange={handleInputName}
         type="text"
         id="name"
         name="name"
         placeholder="Code name"
         defaultValue={defaultData?.name}
+        error={formValidated && inputName === ""}
       />
       <label htmlFor="code">Code*</label>
 
       <StyledCode
-        // value={inputValue}
-        onChange={checkRequiredFields}
+        value={inputCode}
+        onChange={handleInputCode}
         type="text"
         id="code"
         name="code"
         rows="5"
         placeholder="your code"
         defaultValue={defaultData?.code}
+        error={formValidated && inputCode === ""}
       ></StyledCode>
 
       <label htmlFor="description">Description</label>
@@ -101,16 +109,17 @@ const StyledForm = styled.form`
 `;
 
 const StyledInputName = styled.input`
+  border: 2px solid ${(props) => (props.error ? "red" : "initial")};
   height: 2rem;
   border-radius: 0.3rem;
   background-color: var(--light-color);
-  border: none;
 `;
 const StyledCode = styled.textarea`
+  border: 2px solid ${(props) => (props.error ? "red" : "initial")};
   height: 10rem;
   border-radius: 0.3rem;
   background-color: var(--primary-color);
-  border: none;
+
   color: var(--white);
 `;
 
